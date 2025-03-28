175e3831c78cda344484f1225f1fbd448aa9e745
yn <hisa905395@gmail.com>
2025-03-28 13:38:23 +0800

    add basic structure

---
+++ src/test/java/io/eden/springboot_hell/SpringbootHellApplicationTests.java
@@ -0,0 +1,13 @@
+package io.eden.springboot_hell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
